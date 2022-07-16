769c6529e752b14b5f42d25b2789b09a7e3a3ada
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2022-07-16 09:38:41 +0900

    C#から何度も同じバッチファイルを叩きに行く

--- batchExecuter/Program.cs
+++ batchExecuter/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace batchExecuter
 {
@@ -10,6 +10,21 @@ namespace batchExecuter
     {
         static void Main(string[] args)
         {
+            // コマンドライン引数で検索したい単語リスト用のファイルパスを渡している
+            List<string> allLineText = File.ReadAllLines(args[0]).ToList();
+            ProcessStartInfo processInfo;
+
+            foreach (var line in allLineText)
+            {
+                processInfo = new ProcessStartInfo("cmd.exe", $@"/c .\sample.bat { line }");
+                processInfo.CreateNoWindow = true;
+                processInfo.UseShellExecute = false;
+
+                using (Process process = Process.Start(processInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
         }
     }
 }
